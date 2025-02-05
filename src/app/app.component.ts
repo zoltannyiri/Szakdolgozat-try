dad10ae5cb12495e3fc0b59b7d967bfa35a27a9e
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-02-05 15:54:15 +0100

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'LoopHub';
+}
