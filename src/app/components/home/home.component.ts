82aec3fb050a8d20d0eeb1f1777c4344130fcfbf
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-02-27 01:47:26 +0100

    home-fixing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +6,50 @@ import { LoginComponent } from '../login/login.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit, OnInit{
+  @ViewChild('videoPlayer') videoElement!: ElementRef<HTMLVideoElement>;
+
+  currentTextIndex = 0;
+  headerText: string[] = ["hihats?", "drumsets?", "melodies?", "loops?"];
+  displayedText: string = "";
+
+  ngOnInit(): void {
+    setTimeout(() => this.changeHeaderText(), 500);
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const video = this.videoElement.nativeElement;
+      video.muted = true;
+      video.play().catch(err => {
+        console.error("Autoplay failed:", err);
+      });
+    });
+  }
+
+  private changeHeaderText(): void {
+    this.currentTextIndex = (this.currentTextIndex + 1) % this.headerText.length;
+    this.deleteHeaderText();
+  }
+
+  private deleteHeaderText(): void {
+    if (this.displayedText.length === 0) {
+      setTimeout(() => this.typeHeaderText(), 100);
+    } else {
+      this.displayedText = this.displayedText.slice(0, -1);
+      setTimeout(() => this.deleteHeaderText(), 100);
+    }
+  }
+
+  private typeHeaderText(): void {
+    const currentWord = this.headerText[this.currentTextIndex];
+    if (this.displayedText.length === currentWord.length) {
+      setTimeout(() => this.changeHeaderText(), 2000);
+    } else {
+      this.displayedText = currentWord.substring(0, this.displayedText.length + 1);
+      setTimeout(() => this.typeHeaderText(), 100);
+    }
+  }
+
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,32 +11,32 @@ import { RegisterComponent } from '../register/register.component';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  ngOnInit(): void {
-    document.addEventListener('DOMContentLoaded', () => {
-      const burger = document.querySelectorAll('.navbar-burger');
-      const menu = document.querySelectorAll('.navbar-menu');
-      const close = document.querySelectorAll('.navbar-close');
-      const backdrop = document.querySelectorAll('.navbar-backdrop');
+  // ngOnInit(): void {
+  //   document.addEventListener('DOMContentLoaded', () => {
+  //     const burger = document.querySelectorAll('.navbar-burger');
+  //     const menu = document.querySelectorAll('.navbar-menu');
+  //     const close = document.querySelectorAll('.navbar-close');
+  //     const backdrop = document.querySelectorAll('.navbar-backdrop');
 
-      if (burger.length && menu.length) {
-          burger.forEach((burgerElement) => {
-              burgerElement.addEventListener('click', () => {
-                  menu.forEach((menuElement) => {
-                      menuElement.classList.toggle('hidden');
-                  });
-              });
-          });
-      }
+  //     if (burger.length && menu.length) {
+  //         burger.forEach((burgerElement) => {
+  //             burgerElement.addEventListener('click', () => {
+  //                 menu.forEach((menuElement) => {
+  //                     menuElement.classList.toggle('hidden');
+  //                 });
+  //             });
+  //         });
+  //     }
 
-      if (close.length) {
-          close.forEach((closeElement) => {
-              closeElement.addEventListener('click', () => {
-                  menu.forEach((menuElement) => {
-                      menuElement.classList.toggle('hidden');
-                  });
-              });
-          });
-      }
-    });
-  }
+  //     if (close.length) {
+  //         close.forEach((closeElement) => {
+  //             closeElement.addEventListener('click', () => {
+  //                 menu.forEach((menuElement) => {
+  //                     menuElement.classList.toggle('hidden');
+  //                 });
+  //             });
+  //         });
+  //     }
+  //   });
+  // }
 }
