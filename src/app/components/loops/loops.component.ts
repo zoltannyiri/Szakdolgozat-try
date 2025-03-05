3f6510e512d447e5ebd4d135ac3741b484deb7a4
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-03-06 00:33:32 +0100

    loops-component_fixing

--- src/app/components/loops/loops.component.ts
+++ src/app/components/loops/loops.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loops',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './loops.component.html',
   styleUrl: './loops.component.scss'
 })
 export class LoopsComponent {
+  isAdvancedSearchOpen = false;
 
+  toggleAdvancedSearch() {
+    this.isAdvancedSearchOpen = !this.isAdvancedSearchOpen;
+  }
+
+  closeAdvancedSearch(event: Event) {
+    if ((event.target as HTMLElement).classList.contains('modal-overlay')) {
+      this.isAdvancedSearchOpen = false;
+    }
+  }
 }
