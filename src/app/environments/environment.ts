e37c3ff83b688419a47353641b3d9f384f9dcaba
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2025-11-29 15:58:01 +0100

    render update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // local testnél vissza kell irni
-    apiUrl: 'http://localhost:3000',
-    // apiUrl: 'https://szakdolgozat-backend-tg59.onrender.com',
+    // apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://szakdolgozat-backend-tg59.onrender.com',
     googleClientId: '939962725365-jptktomj1t134o18gfud2b98soq86ht2.apps.googleusercontent.com',
 };
